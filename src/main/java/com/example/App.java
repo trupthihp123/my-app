a3b49f5ea512eef2ffd40013e8f071b9ec652e4f
SrinidhiiR <ramdasnidhi@gmail.com>
2026-05-14 15:22:00 +0530

    my app

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
